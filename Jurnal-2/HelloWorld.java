fdbd613cf02d2b7b7d84e1089db11b5d69949d7a
girasmatahari <71244688+girasmatahari@users.noreply.github.com>
2020-09-17 15:19:44 +0700

    HelloWorld.java

---
+++ Jurnal-2/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld{
+
+	public static void main(String[] args) {
+		System.out.println("Nama: Giras");
+		System.out.println("Nim: D3IF-6706204078");
+		System.out.println("Kelas: 44-03");
+	
+	}
+}
